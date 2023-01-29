fb1fe2c7d0bbb75f435af934108946c84256f7ed
KrumovS <simeon.e.krumov@gmail.com>
2023-01-29 19:32:36 +0200

    Initial commit

---
+++ Python Basics/07. For Loop - Lab/number_sequence.py
@@ -0,0 +1,18 @@
+import sys
+
+num = int(input())
+
+max_number = -sys.maxsize
+min_number = sys.maxsize
+
+for i in range(num):
+    current_number = int(input())
+    if current_number > max_number:
+        max_number = current_number
+    if current_number < min_number:
+        min_number = current_number
+
+print(f"Max number: {max_number}")
+print(f"Min number: {min_number}")
+
+
---
+++ Python Basics/07. For Loop - Lab/sum_numbers.py
@@ -0,0 +1,7 @@
+number = int(input())
+total_sum = 0
+
+for i in range(number):
+    num = int(input())
+    total_sum += num
+print(total_sum)
